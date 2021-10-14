f0d56bc7478032e75c01385973e7f135bfa8f30f
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2021-10-14 13:38:04 +0200

    Converting permutation scheme.

--- flippy/pvalue.py
+++ flippy/pvalue.py
@@ -43,54 +43,40 @@ def combine_pvalues(p, combine_with = "tippett"):
     else:
         return -2 * sum(np.log(p))
 
-# def run_permutation_scheme(formula, alternative, stats, B, perm_data, stat_data, M, combine_with, ...):
-#     formula = match_arg(formula, c("exact", "upper_bound", "estimate"))
-#     alternative = match_arg(alternative, c("left_tail", "right_tail", "two_tail"))
-#     nstats = len(stats)
-#     npc = nstats > 1
-#     if npc == True:
-#         altern = "right_tail"
-#     else:
-#         altern = alternative
-#     if len(alternative) == 1:
-#         alternative = np.repeat(alternative, nstats)
-#   
-#     if npc == False:
-#         Tp <- sapply(
-#           X = 0:B,
-#           FUN = get_permuted_statistic,
-#           perm_data = perm_data,
-#           stat_data = stat_data,
-#           stat_fun = stats[[1]],
-#           ...
-#         )
-#     else:
-#         Tp <- stats %>%
-#           purrr::map(function(.x, ...) {
-#             sapply(
-#               X = 0:B,
-#               FUN = get_permuted_statistic,
-#               perm_data = perm_data,
-#               stat_data = stat_data,
-#               stat_fun = .x,
-#               ...
-#             )
-#           }, ...) %>%
-#           purrr::map2(alternative, ~ sapply(
-#             X = 1:(B+1),
-#             FUN = stats2pvalue,
-#             Tp = .x,
-#             M = M,
-#             formula = "upper_bound",
-#             alternative = .y
-#           )) %>%
-#           purrr::transpose() %>%
-#           purrr::simplify_all() %>%
-#           purrr::map_dbl(combine_pvalues, combine_with = combine_with)
-#   
-#     return {
-#         observed: Tp[1],
-#         pvalue: stats2pvalue(1, Tp, M, formula = formula, alternative = altern),
-#         null_distribution: Tp[-1],
-#         permutations: perm_data
-#     }
+def run_permutation_scheme(formula, alternative, stats, B, perm_data, stat_data, M, combine_with, **kwargs):
+    formula = match_arg(formula, c("exact", "upper_bound", "estimate"))
+    alternative = match_arg(alternative, c("left_tail", "right_tail", "two_tail"))
+    nstats = len(stats)
+    npc = nstats > 1
+    if npc == True:
+        altern = "right_tail"
+    else:
+        altern = alternative
+    if len(alternative) == 1:
+        alternative = np.repeat(alternative, nstats)
+
+    if npc == False:
+        Tp = [get_permuted_statistic(i, 
+                                     perm_data = perm_data, 
+                                     stat_data = stat_data, 
+                                     stat_fun = stats[0], 
+                                     **kwargs) for i in np.arange(B+1)]
+    else:
+        Tp = [[get_permuted_statistic(i, 
+                                     perm_data = perm_data, 
+                                     stat_data = stat_data, 
+                                     stat_fun = s, 
+                                     **kwargs) for i in np.arange(B+1)] for s in stats]
+        Tp = [[stats2pvalue(i, Tp = tval, M = M, formula = "upper_bound", alternative = aval) for tval, aval in zip(Tp, alternative)] for i in np.arange(B+1)+1]
+        Tp = [combine_pvalues(tvals, combine_with = combine_with) for tvals in Tp]
+
+    return {
+        observed: Tp[0],
+        pvalue: stats2pvalue(0, Tp, M, formula = formula, alternative = altern),
+        null_distribution: Tp[1:],
+        permutations: perm_data
+    }
+
+def get_permuted_statistic(i, perm_data, stat_data, stat_fun, **kwargs):
+    stat_fun(stat_data, perm_data[, i + 1], **kwargs)
+}
