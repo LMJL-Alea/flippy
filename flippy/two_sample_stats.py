6a7981cbb8eb733eea3f8de17cc1ab56d6f47dbc
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2021-11-05 15:05:44 +0100

    Done converting two sample stats.

--- flippy/two_sample_stats.py
+++ flippy/two_sample_stats.py
@@ -201,97 +201,127 @@ def stat_bs(data, indices1, **kwargs):
     d = m1 - m2
     return d.transpose() @ d - (n1 + n2) / (n1 * n2) * sum(np.diag(vn))
 
-# #' @rdname two-sample-stats
-# #' @export
-# stat_student_ip <- function(data, indices1, ...) {
-#   l <- two_sample_prep(data, indices1)
-#   stat_student_impl(data, l$idx1, l$idx2)
-# }
-# 
-# #' @rdname two-sample-stats
-# #' @export
-# stat_t_ip <- stat_student_ip
-# 
-# #' @rdname two-sample-stats
-# #' @export
-# stat_fisher_ip <- function(data, indices1, ...) {
-#   l <- two_sample_prep(data, indices1)
-#   stat_fisher_impl(data, l$idx1, l$idx2)
-# }
-# 
-# #' @rdname two-sample-stats
-# #' @export
-# stat_f_ip <- stat_fisher_ip
-# 
-# #' @rdname two-sample-stats
-# #' @export
-# stat_bg_ip <- function(data, indices1, ...) {
-#   l <- two_sample_prep(data, indices1)
-#   stat_bg_impl(data, l$idx1, l$idx2)
-# }
-# 
-# #' @rdname two-sample-stats
-# #' @export
-# stat_energy_ip <- function(data, indices1, alpha = 1L, ...) {
-#   l <- two_sample_prep(data, indices1)
-#   stat_energy_impl(data, l$idx1, l$idx2, alpha)
-# }
-# 
-# #' @rdname two-sample-stats
-# #' @export
-# stat_cq_ip <- function(data, indices1, ...) {
-#   l <- two_sample_prep(data, indices1)
-#   stat_cq_impl(data, l$idx1, l$idx2)
-# }
-# 
-# #' @rdname two-sample-stats
-# #' @export
-# stat_mod_ip <- function(data, indices1, ...) {
-#   l <- two_sample_prep(data, indices1)
-#   l <- purrr::cross(l)
-#   dist_values <- purrr::map_dbl(l, ~ getElement(
-#     distObject = data,
-#     rowIndex = .x$idx1,
-#     colIndex = .x$idx2
-#   ))
-#   mean(dist_values)
-# }
-# 
-# #' @rdname two-sample-stats
-# #' @export
-# stat_dom_ip <- function(data, indices1, standardize = TRUE, ...) {
-#   l <- two_sample_prep(data, indices1)
-#   n1 <- length(l$idx1)
-#   n2 <- length(l$idx2)
-# 
-#   ssd1_vec <- purrr::map_dbl(l$idx1, function(idx) {
-#     sum(purrr::map_dbl(l$idx1, ~ getElement(
-#       distObject = data,
-#       rowIndex = idx,
-#       colIndex = .x
-#     ))^2)
-#   })
-#   km1 <- l$idx1[which.min(ssd1_vec)]
-# 
-#   ssd2_vec <- purrr::map_dbl(l$idx2, function(idx) {
-#     sum(purrr::map_dbl(l$idx2, ~ getElement(
-#       distObject = data,
-#       rowIndex = idx,
-#       colIndex = .x
-#     ))^2)
-#   })
-#   km2 <- l$idx2[which.min(ssd2_vec)]
-# 
-#   stat <- getElement(data, km1, km2)
-# 
-#   if (!standardize)
-#     return(stat)
-# 
-#   ssd1 <- min(ssd1_vec)
-#   ssd2 <- min(ssd2_vec)
-#   pooled_variance <- (ssd1 + ssd2) / (n1 + n2 - 2)
-#   stat / sqrt(pooled_variance)
-# }
+@set_doc(two_sample_stats_doc)
+def stat_student_ip(data, indices1, **kwargs):
+    l = two_sample_prep(data, indices1)
+    n1 = len(l["idx1"])
+    n2 = len(l["idx2"])
+    D11 = [[data.get_item(l["idx1"][i], l["idx1"][j]) 
+        for j in range(i + 1, n1)] for i in range(n1)]
+    M11 = (np.array([item for sublist in D11 for item in sublist])**2).mean()
+    D22 = [[data.get_item(l["idx2"][i], l["idx2"][j]) 
+        for j in range(i + 1, n2)] for i in range(n2)]
+    M22 = (np.array([item for sublist in D22 for item in sublist])**2).mean()
+    D12 = [[data.get_item(i, j) for j in l["idx2"]] for i in l["idx1"]]
+    M12 = (np.array(D12)**2).mean()
+    v1 = M11 / 2
+    v2 = M22 / 2
+    numerator = M12 - v1 - v2
+    vn = (v1 / n1 + v2 / n2)
+    if vn < np.sqrt(np.finfo(float).eps):
+        return numerator
+    return numerator / vn
+
+@set_doc(two_sample_stats_doc)
+def stat_t_ip(data, indices1, **kwargs):
+    return stat_student_ip(data, indices1, **kwargs)
+
+@set_doc(two_sample_stats_doc)
+def stat_fisher_ip(data, indices1, **kwarg):
+    l = two_sample_prep(data, indices1)
+    n1 = len(l["idx1"])
+    n2 = len(l["idx2"])
+    D11 = [[data.get_item(l["idx1"][i], l["idx1"][j]) 
+        for j in range(i + 1, n1)] for i in range(n1)]
+    M11 = (np.array([item for sublist in D11 for item in sublist])**2).mean()
+    D22 = [[data.get_item(l["idx2"][i], l["idx2"][j]) 
+        for j in range(i + 1, n2)] for i in range(n2)]
+    M22 = (np.array([item for sublist in D22 for item in sublist])**2).mean()
+    v1 = M11 / 2
+    v2 = M22 / 2
+    vmin = min(v1, v2)
+    vmax = max(v1, v2)
+    if vmin < np.sqrt(np.finfo(float).eps):
+        return vmax
+    return vmax / vmin
+
+@set_doc(two_sample_stats_doc)
+def stat_f_ip(data, indices1, **kwargs):
+    return stat_fisher_ip(data, indices1, **kwargs)
+
+@set_doc(two_sample_stats_doc)
+def stat_bg_ip(data, indices1, **kwargs):
+    l = two_sample_prep(data, indices1)
+    n1 = len(l["idx1"])
+    n2 = len(l["idx2"])
+    D11 = [[data.get_item(l["idx1"][i], l["idx1"][j]) 
+        for j in range(i + 1, n1)] for i in range(n1)]
+    M11 = np.array([item for sublist in D11 for item in sublist]).mean()
+    D22 = [[data.get_item(l["idx2"][i], l["idx2"][j]) 
+        for j in range(i + 1, n2)] for i in range(n2)]
+    M22 = np.array([item for sublist in D22 for item in sublist]).mean()
+    D12 = [[data.get_item(i, j) for j in l["idx2"]] for i in l["idx1"]]
+    M12 = np.array(D12).mean()
+    return (M11 - M12)**2 + (M22 - M12)**2
+
+@set_doc(two_sample_stats_doc)
+def stat_energy_ip(data, indices1, alpha = 1, **kwargs):
+    l = two_sample_prep(data, indices1)
+    n1 = len(l["idx1"])
+    n2 = len(l["idx2"])
+    D11 = [[data.get_item(i, j) for j in l["idx1"]] for i in l["idx1"]]
+    M11 = (np.array(D11)**alpha).mean()
+    D22 = [[data.get_item(i, j) for j in l["idx2"]] for i in l["idx2"]]
+    M22 = (np.array(D22)**alpha).mean()
+    D12 = [[data.get_item(i, j) for j in l["idx2"]] for i in l["idx1"]]
+    M12 = (np.array(D12)**alpha).mean()
+    return M12 - (M11 + M22) / 2
+
+@set_doc(two_sample_stats_doc)
+def stat_cq_ip(data, indices1, **kwargs):
+    l = two_sample_prep(data, indices1)
+    n1 = len(l["idx1"])
+    n2 = len(l["idx2"])
+    D11 = [[data.get_item(i, j) for j in l["idx1"] if j != i] for i in l["idx1"]]
+    M11 = (np.array(D11)).mean()
+    D22 = [[data.get_item(i, j) for j in l["idx2"] if j != i] for i in l["idx2"]]
+    M22 = (np.array(D22)).mean()
+    D12 = [[data.get_item(i, j) for j in l["idx2"]] for i in l["idx1"]]
+    M12 = (np.array(D12)).mean()
+    return M11 + M22 - 2 * M12
+  
+@set_doc(two_sample_stats_doc)
+def stat_mod_ip(data, indices1, **kwargs):
+    l = two_sample_prep(data, indices1)
+    n1 = len(l["idx1"])
+    n2 = len(l["idx2"])
+    D12 = [[data.get_item(i, j) for j in l["idx2"]] for i in l["idx1"]]
+    M12 = (np.array(D12)).mean()
+    return M12
+
+@set_doc(two_sample_stats_doc)
+def stat_dom_ip(data, indices1, standardize = True, **kwargs):
+    l = two_sample_prep(data, indices1)
+    n1 = len(l["idx1"])
+    n2 = len(l["idx2"])
+    
+    D11 = [[data.get_item(i, j) for j in l["idx1"]] for i in l["idx1"]]
+    ssd1_vec = (np.array(D11)**2).sum(axis = 0)
+    km1 = l["idx1"][ssd1_vec.argmin()]
+    
+    D22 = [[data.get_item(i, j) for j in l["idx2"]] for i in l["idx2"]]
+    ssd2_vec = (np.array(D22)**2).sum(axis = 0)
+    km2 = l["idx2"][ssd2_vec.argmin()]
+    
+    stat = data.get_item(km1, km2)
+    
+    if not standardize:
+        return stat
+    
+    ssd1 = min(ssd1_vec)
+    ssd2 = min(ssd2_vec)
+    vp = (ssd1 + ssd2) / (n1 + n2 - 2)
+    return stat / np.sqrt(vp)
 
 def two_sample_prep(data, indices1):
     if isinstance(data, list):
@@ -301,5 +331,7 @@ def two_sample_prep(data, indices1):
     else:
         sys.exit("The `data` input should be of class either list or dist.")
     indices1 = list(indices1)
+    indices1.sort()
     indices2 = list(set(range(n)) - set(indices1))
+    indices2.sort()
     return {"idx1": indices1, "idx2": indices2}
